ee22e5d92aaf53487cb8280452e8cc2f10d8e95d
Bryan Maynard <bsgbryan@gmail.com>
2018-02-24 17:16:30 -0600

    Created a lib dir and moved builder.js into it
    I don't want a bunch of files to pile up at the root, so I created a `lib` folder.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = (name, total_volume, pipeline) => {
-  const level = require('./builder')(name, total_volume, pipeline)
+  const level = require('./lib/builder')(name, total_volume, pipeline)
 
   for (let pipe of level.pipeline) {
     require(`./recipies/${level.name}/delegates/${pipe}`)(level)
