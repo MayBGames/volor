cc463d102b0e1d6e5e1845732c20260823d38db2
Bryan Maynard <bsgbryan@gmail.com>
2018-03-02 11:19:22 -0600

    Write output to json file
    Instead of printing thigns to the console, it's more helpful now to have volor save it's work to a json file.

--- cli.js
+++ cli.js
@@ -1,7 +1,16 @@
 #! /usr/bin/env node
 
+const fs      = require('fs')
 const args    = require('./arguments')
 const builder = require('./index')
 const built   = builder('DEFAULT', 1000, [ 'path', 'tiles', 'landscape' ])
 
-console.log(built)
+fs.writeFile(`./tmp/${Date.now()}.json`, JSON.stringify(built, null, 2), 'utf8', (err) => {
+  if (err) {
+    console.log(err)
+    process.exit(1)
+  } else {
+    console.log('Output saved')
+    process.exit(0)
+  }
+})
